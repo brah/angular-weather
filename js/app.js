b5eacf0963432cfef967338b4ab868afcda7e47e
qbabin <q.babin@gmail.com>
2014-08-03 11:39:54 -0700

    Date object is formatted and showing in the view

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-angular.module('myApp', [])
-.controller('mainCtrl', function($scope, $timeout){
+angular.module('weatherApp', ['ngRoute'])
+.controller('mainCtrl', function($scope, $timeout) {
 	//Building a date object
 	$scope.date = {};
 
 	//Update function
 	var updateTime = function() {
-		$scope.date.raw = Date();
+		$scope.date.raw = new Date();
 		$timeout(updateTime, 1000);
 
 	}
 	//Every second mainCtrl is on the view, updateTime() will be ran to update $scope.date.raw
 	updateTime();
-})
+});
